c39fb245dc8045fdb591457cd566d5d0a6a333e0
Alex Ball <alex@ameryn.com>
2017-09-14 23:40:23 -0400

    Postgres repo, User migration

--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,9 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
+# For development, we disable any cache and enable debugging and code reloading.
 #
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
+# The watchers configuration can be used to run external watchers to your application.
+# For example, we use it with brunch.io to recompile .js and .css sources.
 config :rumbl, Rumbl.Endpoint,
   http: [port: 4000],
   debug_errors: true,
--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
+# We don't run a server during test. If one is required, you can enable the server option below.
 config :rumbl, Rumbl.Endpoint,
   http: [port: 4001],
   server: false
